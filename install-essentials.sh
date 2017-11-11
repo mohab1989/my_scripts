4e4957942c97efa31e91c406854fa0711ff942ef
mohab <mohabe1989@Gmail.com>
2017-11-11 10:31:27 +0200

    adding install-essentials.sh script

---
+++ install-essentials.sh
@@ -0,0 +1,8 @@
+# Install gcc/g++
+sudo apt-get install build-essential
+
+# To be able to use add-apt-repository you may need to install software-properties-common
+sudo apt-get install software-properties-common
+
+# Install python/pip 2/3
+sudo apt-get install python-dev python-pip python3-dev python3-pip
