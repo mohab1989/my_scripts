b816787128167a8dc9eaacd5262c3120cfe27f09
mohab <mohabe1989@Gmail.com>
2017-11-30 20:28:25 +0200

    adding plug-vim to neovim installation YCM not working yet

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,8 +1,14 @@
 # Install gcc/g++
-sudo apt-get install build-essential
+sudo apt-get install build-essential cmake
+
+# Install clang llvm compilers
+sudo apt-get install clang-4.0
 
 # To be able to use add-apt-repository you may need to install software-properties-common
 sudo apt-get install software-properties-common
 
 # Install python/pip 2/3
 sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+sudo -H pip install --upgrade pip
+sudo -H pip3 install --upgrade pip
