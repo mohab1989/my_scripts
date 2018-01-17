691ee9fadf0a77f59fc97cc95c6e0c05b1b293bc
mohab <mohabe1989@gmail.com>
2018-01-17 20:19:07 +0200

    fix installing gtest and gmock

--- install_essentials.sh
+++ install_essentials.sh
@@ -2,10 +2,15 @@
 sudo apt-get install build-essential cmake
 
 # Install clang llvm compilers
+clang_version=5.0
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main"
+sudo apt-add-repository "deb http://apt.llvm.org/xenial/llvm-toolchain-xenial-${clang_version} main"
 sudo apt-get update
-sudo apt-get -y clang-5.0
+sudo apt-get install clang-${clang_version}
+
+# Create clang and clang++ link
+sudo link /usr/bin/clang-${clang_version}  /usr/bin/clang 
+sudo link /usr/bin/clang++-${clang_version}  /usr/bin/clang++ 
 
 # To be able to use add-apt-repository you may need to install software-properties-common
 sudo apt-get install software-properties-common
@@ -13,19 +18,20 @@ sudo apt-get install software-properties-common
 # Install python/pip 2/3
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 
-sudo -H pip install --upgrade pip
-sudo -H pip3 install --upgrade pip
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
 
 # Install gtest and gmock
 # Installs the headers at /usr/include/(gtest\gmock)
 sudo apt-get install libgtest-dev google-mock
 
 # Check libgtest.a exists already
-if [ ! -f /usr/lib/libgtest.a ];then
-	
-	# build gtest
+if [ ! -f /usr/lib/libgtest.a ];then	
+	# generate gtest build files
 	cd /usr/src/gtest
-	mkdir build && cd build && cmake ..
+	sudo mkdir build && cd build && sudo cmake ..
+    # build gtest
+    sudo make
 
 	# Move gtest libs to /usr/lib
 	sudo mv *.a /usr/lib
@@ -33,11 +39,13 @@ if [ ! -f /usr/lib/libgtest.a ];then
 fi
 
 # Check libgmock.a exists already
-if [ ! -f /usr/lib/libgmock.a ];then 
-	
-	# build gmock
+if [ ! -f /usr/lib/libgmock.a ];then 	
+	# generate gmock build files
 	cd /usr/src/gmock
-	mkdir build && cd build && cmake ..
+	sudo mkdir build && cd build && sudo cmake ..
+    
+    # build gmock
+    sudo make
 
 	# Move gmock libs to /usr/lib
 	sudo mv *.a /usr/lib
