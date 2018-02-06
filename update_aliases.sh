ce51fe493226f85c6c42af9b5d2840c41a7e12ed
Mohab Meshref <mohabe1989@gmail.com>
2018-02-06 16:20:17 +0200

    add alias for rm -I

--- update_aliases.sh
+++ update_aliases.sh
@@ -11,4 +11,6 @@ echo "overwrite: $overwrite"
 
 echo "
 alias clcmake='cmake -G \'Unix Makefiles\' -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++'
-alias gcmake='cmake -G \'Unix Makefiles\' -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++'" >> $alias_file
+alias gcmake='cmake -G \'Unix Makefiles\' -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++'
+#porhibit accidental removes
+alias rmi='rm -I'" >> $alias_file
